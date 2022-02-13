586e60650ea67325ac32df740146f5bd01b87192
Anton Guinto <antonvinceguinto@gmail.com>
2022-02-13 14:53:25 +0800

    ptch: Adjust padding of category label

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@
 export default function Footer() {
   return (
       <div className="text-center h-32 flex items-center justify-center bg-black">
-          <p className="text-xl text-white">
+          <p className="text-sm text-white">
               &copy; Copyright 2022 <span className='font-bold'>Wetflix</span>
           </p>
       </div>
--- components/PopularMovie.js
+++ components/PopularMovie.js
@@ -2,8 +2,8 @@ import MovieCard from './MovieCard'
 
 export default function PopularMovie({ movies }) {
 	return (
-		<div className='container max-w-full mx-auto pb-10 pl-16 '>
-			<h1 className='text-white text-2xl pt-8 pb-5 font-bold'>Popular Movies</h1>
+		<div className='container max-w-full mx-auto pb-10'>
+			<h1 className='text-white text-2xl pt-8 pb-5 font-bold pl-8'>Popular Movies</h1>
 			<div className='flex overflow-x-scroll scrollbar'>
 				{movies.map((movie) => <MovieCard movie={movie} key={movie.id} />)}
 			</div>
